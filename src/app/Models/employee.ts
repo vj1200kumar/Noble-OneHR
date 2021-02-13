014549c31092d32f9cb872cf824689122033f12a
Ashish <ashish@liekotechnologies.com>
2021-02-13 07:26:45 +0000

    Changes from Ashish

---
+++ src/app/Models/employee.ts
@@ -0,0 +1,73 @@
+export class Employee {
+    EmployeeId;
+    CompanyName;
+    Name;
+    EmployeeCode;
+    Gender;
+    BirthDate;
+    MobileNo;
+    EmailId;
+    BloodGroup;
+    Nationality;
+    NoOfDependants;
+    Status;
+    CompanyDetails: {
+        CompanyNo;
+        CompanyName;
+        Position;
+        EntryDate;
+        HiringDate;
+    };
+    AddressDetails: {
+        AddressType;
+        Address1;
+        Address2;
+        Street;
+        City;
+        district;
+    };
+    IqamaDetails: {
+        IqamaId;
+        BoarderNo;
+        IsIqamaExpired;
+        DateOfExpiry;
+    };
+    PassportDetails: {
+        PassportNumber;
+        PlaceOfIssue;
+        IssueDate;
+        DateOfExpiry;
+    };
+    EmergencyContactDetails: {
+        ContactName;
+        Relation;
+        Email;
+        ConatctNumber;
+    };
+    BankingAndInsuranceDetails: {
+        BankAccountNoPayroll;
+        PersonalBankAccount;
+        InsuranceCoverage;
+    };
+    SalaryDetails: {
+        TotalSalary;
+        BasicSalary;
+        HosusingAlloawance;
+        TransportAlloawance;
+        Deductions;
+        OtherAlloawances;
+    };
+    ActiveVacationDetails: {
+        VacationType;
+        Date;
+        VacationStartDate;
+        VacationEndDate;
+        TotalVacationDays;
+        Notes;
+    };
+    VacationDetails: {
+        VacationBalance;
+        LastVacationDate;
+        LastResumptionDate;
+    };
+}
