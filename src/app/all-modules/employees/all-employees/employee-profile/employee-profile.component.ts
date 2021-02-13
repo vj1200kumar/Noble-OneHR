4c7cfc851bed4cf719ce9f64cc6481eae56dbf99
Vijay Kumar Singh <vijay@citizn.world>
2021-02-13 17:04:54 +0530

    Employee profile with dynamic data

--- src/app/all-modules/employees/all-employees/employee-profile/employee-profile.component.ts
+++ src/app/all-modules/employees/all-employees/employee-profile/employee-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ToastrService } from "ngx-toastr";
+import { ActivatedRoute, Router } from '@angular/router';
+import { AllModulesService } from "src/app/all-modules/all-modules.service";
 
 @Component({
   selector: "app-employee-profile",
@@ -8,16 +10,36 @@ import { ToastrService } from "ngx-toastr";
   styleUrls: ["./employee-profile.component.css"],
 })
 export class EmployeeProfileComponent implements OnInit {
+  public lstEmployee: any[];
   public addEmployeeForm: FormGroup;
   constructor(
+    private srvModuleService: AllModulesService,
     private toastr: ToastrService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private _Activatedroute: ActivatedRoute,
+    private _router: Router
   ) {}
+
+  sub;
+  currEmpData;
 
   ngOnInit() {
     this.addEmployeeForm = this.formBuilder.group({
       client: ["", [Validators.required]],
     });
+
+    this.sub=this._Activatedroute.paramMap.subscribe((params: any) => { 
+      this.srvModuleService.get("this.url").subscribe((data:any) => {
+        const empDataIdx = data.findIndex((data) => data?.Id == params.params.id);
+        const empData = data[empDataIdx];
+        this.currEmpData = empData;
+        console.log('currEmpData', this.currEmpData);
+      });
+   });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
   onSubmit() {
--- src/app/all-modules/employees/employees-routing.module.ts
+++ src/app/all-modules/employees/employees-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
         component: EmployeeListComponent
       },
       {
-        path: 'employeeprofile',
+        path: 'employeeprofile/:id',
         component: EmployeeProfileComponent
       },
       {
